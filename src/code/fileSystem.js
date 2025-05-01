d97ce177f9eeda3a9f969e3559eceae0a637e6be
Iannacus <ian.rosasmal@gmail.com>
2025-05-01 17:28:51 -0600

    update core v 0.2

---
+++ src/code/fileSystem.js
@@ -0,0 +1,14 @@
+import { FileSystem } from "@iannaucs/dimacheri/extra-components";
+
+<FileSystem
+  tree={`
+    - src/
+    -- server.ts
+    -- model/
+    --- auth.model.ts
+    -- views/
+    --- auth.view.ts
+    -- controllers/
+    --- auth.controller.ts
+`}
+/>;
